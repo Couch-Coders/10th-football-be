a03ca9009345c14fa5f27f667d2900c306c23685
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-08-25 00:07:31 +0900

    chore: modify entity

--- src/main/java/couch/football/domain/match/Subscription.java
+++ src/main/java/couch/football/domain/match/Subscription.java
@@ -1,4 +1,33 @@
 package couch.football.domain.match;
 
+import couch.football.domain.member.Member;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
+import static javax.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@ToString
+@NoArgsConstructor
+@Table(name = "subscriptions")
 public class Subscription {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "subscription_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "match_id")
+    private Match match;
+
 }
--- src/main/java/couch/football/domain/stadium/Stadium.java
+++ src/main/java/couch/football/domain/stadium/Stadium.java
@@ -1,4 +1,54 @@
 package couch.football.domain.stadium;
 
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stadiums")
 public class Stadium {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stadium_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "stadium")
+    private List<File> files = new ArrayList<>();
+
+    private String name;
+
+    @Lob
+    private String content;
+
+    private boolean parking;
+
+    private boolean rental;
+
+    private String address;
+
+    private Long likeCount;
+
+    private LocalDateTime createAt;
+
+    @Builder
+
+    public Stadium(List<File> files, String name, String content, boolean parking, boolean rental, String address, Long likeCount) {
+        this.files = files;
+        this.name = name;
+        this.content = content;
+        this.parking = parking;
+        this.rental = rental;
+        this.address = address;
+        this.likeCount = likeCount;
+        this.createAt = LocalDateTime.now();
+    }
 }
