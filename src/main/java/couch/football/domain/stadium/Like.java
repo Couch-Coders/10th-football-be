b66865b217372905145efc8fae6d2b7489077d4c
minbo2002 <minbo2002@naver.com>
2022-08-24 18:41:01 +0900

    chore: modify entity

--- src/main/java/couch/football/domain/stadium/Like.java
+++ src/main/java/couch/football/domain/stadium/Like.java
@@ -1,4 +1,27 @@
 package couch.football.domain.stadium;
 
+import couch.football.domain.member.Member;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "likes")
 public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stadium_id", nullable = false)
+    private Stadium stadium;
 }
--- src/main/java/couch/football/domain/stadium/Stadium.java
+++ src/main/java/couch/football/domain/stadium/Stadium.java
@@ -1,4 +1,54 @@
 package couch.football.domain.stadium;
 
+import couch.football.domain.match.Match;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "stadiums")
 public class Stadium {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "stadium_id")
+    private Long id;
+
+    @OneToMany(mappedBy = "matches", fetch = FetchType.LAZY)
+    private List<Match> matches = new ArrayList<>();
+
+    @OneToMany(mappedBy = "files", fetch = FetchType.LAZY)
+    private List<File> files = new ArrayList<>();
+
+    @OneToMany(mappedBy = "likes", fetch = FetchType.LAZY)
+    private List<Like> likes = new ArrayList<>();
+
+    @Column(nullable = false)
+    private String name;
+
+    @Lob
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = true)
+    private boolean parking;
+
+    @Column(nullable = true)
+    private boolean rental;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "like_count", nullable = true)
+    private Long likeCount;
+
+    @Column(name = "create_at", nullable = false)
+    private LocalDateTime createAt;
+
 }
