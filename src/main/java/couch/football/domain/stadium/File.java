cda258ce06bb136e2515216f9e648bff112d1616
minbo2002 <minbo2002@naver.com>
2022-08-24 18:43:51 +0900

    feat: create entity

---
+++ src/main/java/couch/football/domain/stadium/File.java
@@ -0,0 +1,26 @@
+package couch.football.domain.stadium;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "files")
+public class File {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "file_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "stadium_id", nullable = false)
+    private Stadium stadium;
+
+    @Column(name = "image_url" ,nullable = false)
+    private String imageUrl;
+}
