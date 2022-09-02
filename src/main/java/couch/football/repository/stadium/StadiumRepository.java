edae772ac162a8871e49669fff1444a5a87c2f93
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-09-02 23:19:02 +0900

    feat: search address in stadium

--- src/main/java/couch/football/repository/stadium/StadiumRepository.java
+++ src/main/java/couch/football/repository/stadium/StadiumRepository.java
@@ -3,5 +3,5 @@ package couch.football.repository.stadium;
 import couch.football.domain.stadium.Stadium;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface StadiumRepository extends JpaRepository<Stadium, Long> {
+public interface StadiumRepository extends JpaRepository<Stadium, Long>, StadiumRepositoryCustom{
 }
