e4c72cb820bff9f27cf0a5da999bbbeda7bc7a6b
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-09-05 12:44:33 +0900

    chore: add @Transactional

--- src/main/java/couch/football/service/stadium/StadiumService.java
+++ src/main/java/couch/football/service/stadium/StadiumService.java
@@ -4,12 +4,14 @@ import couch.football.repository.stadium.StadiumRepository;
 import couch.football.response.stadium.StadiumSearchResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class StadiumService {
 
     private final StadiumRepository stadiumRepository;
