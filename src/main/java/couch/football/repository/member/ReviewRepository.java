34331154b26368cd9e71efeeee18b0daedeee3f3
Minhyeok Kang <kmhyeok0816@gmail.com>
2022-09-12 15:52:09 +0900

    chore: delete files

--- src/main/java/couch/football/repository/member/ReviewRepository.java
+++
@@ -1,7 +0,0 @@
-package couch.football.repository.member;
-
-import couch.football.domain.match.Review;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReviewRepository extends JpaRepository<Review, Long> {
-}
